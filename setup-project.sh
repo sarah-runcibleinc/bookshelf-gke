36a273f90e75d5f5b29319d3ea987f4941ca0783
Don McCasland <donmccasland@google.com>
2019-12-11 15:15:30 -0800

    Add permissions set up

--- setup-project.sh
+++ setup-project.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 set -x
 export PROJECT_ID=$1
+export PROJECT_NUM=$(gcloud projects list | grep $PROJECT_ID | egrep -o '[0-9]+$')
 gcloud config set project $PROJECT_ID
 gcloud services enable container.googleapis.com
 gcloud services enable containerregistry.googleapis.com
 gcloud beta services enable cloudbuild.googleapis.com
+gcloud project add-iam-policy-binding $PROJECT_ID \
+  --member serviceAccount:$PROJECT_NUM@cloudbuild.gserviceaccount.com \
+  --role roles/appengine.appAdmin \
+  --role roles/container.admin \
+  --role roles/storage.admin
 gcloud auth configure-docker -q
 gcloud container clusters create bookshelf \
     --num-nodes=2 --zone=us-central1-c
